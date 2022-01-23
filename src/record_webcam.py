11d5db02677f938071e7373b79250f87d70d0f20
Andrey-Korn <andreykornilovich@gmail.com>
2022-01-22 16:24:01 -0800

    initial recorder attempt, and camera calibration

--- src/record_webcam.py
+++ src/record_webcam.py
@@ -1,25 +1,55 @@
 import cv2 as cv
 import webcam
+import argparse
+import time
 
-config_file = 'camera_config.yaml'
-camera_id = 1
+# specify command line args
+parser = argparse.ArgumentParser(description='Monitor or record a webcam feed.')
+parser.add_argument('-r', dest='record', 
+                    help='record camera feed to file')
+parser.add_argument('--config', 
+                    help='change camera config file')
+args = parser.parse_args()
+# print(args)
 
-camera = webcam.webcam(config_file, camera_id)
+# default camera config, or one from arg parse
+config_file = 'camera_config_1080.yaml'
+if args.config is not None:
+    config_file = args.config
+
+# create a camera object with the given config
+camera = webcam.webcam(config_file)
+
+video_out = None
+if args.record is not None:
+    # fourcc = cv.VideoWriter_fourcc('m', 'p', '4', 'v')
+    # fourcc = cv.VideoWriter_fourcc(*'mp4v')
+    fourcc = cv.VideoWriter_fourcc(*'XVID')
+    video_out = cv.VideoWriter(args.record, fourcc, camera.fps, (camera.frame_width, camera.frame_height))
+
+start_t = time.time()
 
 while True:
     # grab frame
     ret, frame = camera.vid.read()
 
     if not ret:
-        print('No frame found! Exiting...')
+        print('End of video stream, exiting...')
         break
 
     # display frame
     cv.imshow(camera.window_name, frame)
+
+    # if video_out is not None:
+    video_out.write(frame)
+
     if cv.waitKey(1) == ord('q'):
         break
+
+end_t = time.time()
 
 
 camera.print_camera_settings()
 camera.vid.release()
+video_out.release()
 cv.destroyAllWindows()
