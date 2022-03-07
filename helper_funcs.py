fixed bug in calculation of ball detection accuracy. added dilation step to ball detection for slightly increased accuracy

helper_funcs.py
@@ -39,7 +39,8 @@ color_map = {
 
 
 def draw_text(img: np.ndarray, text:str, position: tuple, BGR_color: tuple) -> None:
-    
+    """ Draws text of color BGR_color to img at position """
+
     new_img = cv.putText(img, text, position, cv.FONT_HERSHEY_SIMPLEX, 2, BGR_color, 3)
     img = new_img
 
@@ -89,9 +90,10 @@ def detect_blue_ball(src: np.ndarray) -> tuple:
     no_green_red = cv.bitwise_and(no_green, no_green, mask=no_red)
 
     kernel = np.ones((3,3), np.uint8)
-    eroded = cv.erode(no_green_red, kernel, iterations=1)
+    # eroded = cv.erode(no_green_red, kernel, iterations=1)
+    eroded_dilated = erode_and_dilate(no_green_red, 3)
 
-    final_image = eroded
+    final_image = eroded_dilated
 
     circles = cv.HoughCircles(final_image, cv.HOUGH_GRADIENT, 1, 50, param1=30, param2=15, minRadius=3, maxRadius=50)
 

main.py
@@ -60,7 +60,7 @@ while cap.isOpened():
         ball_pos = new_ball_pos
 
     # If ball not found during this cycle, tally a detection failure (currently stops at frame 769, as this is when the ball falls into a hole in the training video)
-    if frame_count < 769:
+    if ball_pos is None and frame_count < 769:
         missed_frames_ball += 1
 
 
@@ -92,4 +92,4 @@ cv.destroyAllWindows()
 
 # Print statistics to terminal
 print(f"Number of frames where ball was missed: {missed_frames_ball}")
-print(f"Ball detection rate: {np.around((1 - missed_frames_ball / frame_count), decimals=3) * 100}%")
+print(f"Ball detection rate: {np.around((1 - missed_frames_ball / frame_count), decimals=4) * 100}%")