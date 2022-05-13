efa825cb78b6c2180971e4d6763229df0d1c0e36
Andrey-Korn <andreykornilovich@gmail.com>
2022-05-12 22:37:31 -0700

    minimum enclosing circle ball detection

--- src/feature_detector.py
+++ src/feature_detector.py
@@ -1,63 +1,12 @@
+from os import error
 import cv2 as cv
 import numpy as np
+from numpy.lib import utils
+from webcam import webcam
 
-from cv_utils import *
+from utils import *
 
 # bring in file vs webcam choice
-
-def erode_and_dilate(src: np.ndarray, kernel_size: int, iterations: int = 1) -> np.ndarray:
-    """ Performs a series of erosions followed by dilations on src image """
-    
-    erosion_kernel = np.ones((  kernel_size, kernel_size), np.uint8)
-    dilation_kernel = (         kernel_size, kernel_size)
-
-    while iterations:
-        eroded = cv.erode(src, erosion_kernel, iterations=1)
-        src = cv.dilate(eroded, dilation_kernel, iterations=1)
-        iterations -= 1
-
-    return src
-
-def ball_error(ball_pos, target):
-    return (target[0] - ball_pos[0], target[1] - ball_pos[1])
-
-
-def detect_blue_ball(src: np.ndarray) -> tuple:
-    """ 
-    Detects the blue ball in an image. 
-    Returns: 
-        * tuple (x, y, rad)
-        * if no ball detected, returns None
-    """
-
-    blur = cv.GaussianBlur(src, (7, 7), cv.BORDER_DEFAULT)
-    blue_channel = blur[:,:,0]
-    ret, green_mask = cv.threshold(blur[:,:,1], 50, 255, cv.THRESH_BINARY_INV)
-    ret, red_mask = cv.threshold(blur[:,:,2], 15, 255, cv.THRESH_BINARY_INV)
-    masked = cv.inRange(blue_channel, 20, 150)
-    no_green = cv.bitwise_and(masked, masked, mask=green_mask)
-    no_red = cv.bitwise_and(masked, masked, mask=red_mask)
-    no_green_red = cv.bitwise_and(no_green, no_green, mask=no_red)
-
-    kernel = np.ones((3,3), np.uint8)
-    # eroded = cv.erode(no_green_red, kernel, iterations=1)
-    eroded_dilated = erode_and_dilate(no_green_red, 3)
-
-    final_image = eroded_dilated
-
-    circles = cv.HoughCircles(final_image, cv.HOUGH_GRADIENT, 1, 50, param1=30, param2=15, minRadius=3, maxRadius=50)
-
-    if circles is not None:
-        circles = np.uint16(np.around(circles))
-        ball = circles[0,:][0]
-        return (
-            ball[0],
-            ball[1],
-            ball[2]
-        )
-    else:
-        return None
-
 
 def detect_path(img: np.ndarray) -> np.ndarray:
     """
@@ -95,95 +44,185 @@ def detect_path(img: np.ndarray) -> np.ndarray:
         if near_center and cv.arcLength(cnt, True) > 10_000:
             print(cnt)
             return cnt
-
     return
 
 
-# Open video feed/file
-video_path = video_path_map["blue_bright"]
-cap = cv.VideoCapture(video_path)
+def find_maze_corner(src: np.ndarray):
+    # search for top left
 
-# Variables for recording detected object state
-path, ball_pos = None, None
+    # search for top right
+    # search for bot left
+    # search for bot right
+    pass
 
-# For calculating statistics (object detection accuracy, framerate)
-frame_count = 0
-missed_frames_ball = 0
+def perspective_transform(src: np.ndarray):
+    pass
 
-# Main loop - object detection and labeling for each video frame
-while cap.isOpened():
+def erode_and_dilate(src: np.ndarray, kernel_size: int, iterations: int = 1) -> np.ndarray:
+    """ Performs a series of erosions followed by dilations on src image """
+    
+    erosion_kernel = np.ones((  kernel_size, kernel_size), np.uint8)
+    dilation_kernel = (         kernel_size, kernel_size)
 
-    ### Step 1: Get video frame
-    ret, frame = cap.read()
-    if not ret:
-        print("Error: video frame not loaded.")
-        break
-    frame_count += 1
+    while iterations:
+        eroded = cv.erode(src, erosion_kernel, iterations=1)
+        src = cv.dilate(eroded, dilation_kernel, iterations=1)
+        iterations -= 1
 
-    frame = frame[100:1080, 390:1560,:]  # Crop video frame to only desired area
+    return src
 
 
-    ### STEP 2: Detect objects
+def detect_blue_ball(src: np.ndarray) -> tuple:
+    """ 
+    Detects the blue ball in an image. 
+    Returns: 
+        * tuple (x, y, rad)
+        * if no ball detected, returns None
+    """
+    blur = cv.GaussianBlur(src, (7, 7), cv.BORDER_DEFAULT)
+    blue_channel = blur[:,:,0]
+    ret, green_mask = cv.threshold(blur[:,:,1], 50, 255, cv.THRESH_BINARY_INV)
+    ret, red_mask = cv.threshold(blur[:,:,2], 15, 255, cv.THRESH_BINARY_INV)
+    masked = cv.inRange(blue_channel, 20, 150)
+    no_green = cv.bitwise_and(masked, masked, mask=green_mask)
+    no_red = cv.bitwise_and(masked, masked, mask=red_mask)
+    no_green_red = cv.bitwise_and(no_green, no_green, mask=no_red)
 
-    ## 2.1: Path
-    # Find current path outline/contour (only every 3rd frame)
-    if frame_count % 3 == 0:
-        new_path = detect_path(frame)
-        if new_path is not None:
-            path = new_path
+    kernel = np.ones((3,3), np.uint8)
+    eroded_dilated = erode_and_dilate(no_green_red, 3)
+    # eroded_dilated = erode_and_dilate(no_green_red, 1)
 
-    ## 2.2: Ball
-    # Find current ball position
-    if ball_pos is None:    # If ball was not detected during last cycle, search entire video frame
-        ball_pos = detect_blue_ball(frame)
-    else:                   # If ball was detected last cycle, search only the area surrounding the most recently recorded ball position
-        area_size = 100
-        x_min_offset = min(0, ball_pos[0] - area_size)
-        y_min_offset = min(0, ball_pos[1] - area_size)
-        x_min, x_max = max(0, ball_pos[0] - area_size), ball_pos[0] + area_size
-        y_min, y_max = max(0, ball_pos[1] - area_size), ball_pos[1] + area_size
-        new_ball_pos = detect_blue_ball(frame[y_min:y_max, x_min:x_max, :])
+    final_image = eroded_dilated
+
+    # uncomment to see ball segmentation
+    cv.imshow('ball_mask', final_image)
+
+    # method 2: min enclosing circle
+    circles = []
+    contours, hierarchy = cv.findContours(eroded_dilated, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    for c in contours:
+        (x,y), r = cv.minEnclosingCircle(c)
+        # reject contours that do not meet ball radius
+        if r > 15 and r < 30:
+            circles.append((x, y, r))
+            # print(f'x: {x} y: {y} r: {r}')
+            return [int(x), int(y), int(r)]
+
+    return None
+
+    # Method 1: Hough Circles 
+    # circles = cv.HoughCircles(final_image, cv.HOUGH_GRADIENT, 1, 50, param1=30, param2=15, minRadius=2, maxRadius=50)
+    # if circles is not None:
+        # circles = np.uint16(np.around(circles))
+        # ball = circles[0,:][0]
+        # print(ball)
+        # return (
+            # ball[0],
+            # ball[1],
+            # ball[2]
+        # )
+    # else:
+        # return None
+
+
+def main():
+    # Open video feed/file
+    video_path = video_path_map["blue_bright"]
+    # cap = cv.VideoCapture(video_path)
+    # cap = cv.VideoCapture()
+    conf = config_files['camera_1080']
+    camera = webcam(conf)
+    settings = read_yaml(conf)
+    window_name = settings['window_name']
+
+
+    # Variables for recording detected object state
+    path, ball_pos = None, None
+
+    # For calculating statistics (object detection accuracy, framerate)
+    frame_count = 0
+    missed_frames_ball = 0
+
+    # Main loop - object detection and labeling for each video frame
+    while camera.vid.isOpened():
+
+        ### Step 1: Get video frame
+        ret, frame = camera.read_frame()
+        if not ret:
+            print("Error: video frame not loaded.")
+            break
+        frame_count += 1
+
+        # Crop video frame to only desired area
+        frame = crop_frame(frame, settings['x_frame'], settings['y_frame'])
+
+
+        ### STEP 2: Detect objects
+
+        ## 2.1: Path
+        # Find current path outline/contour (only every 3rd frame)
+        if frame_count % 3 == 0:
+            new_path = detect_path(frame)
+            if new_path is not None:
+                path = new_path
+
+        ## 2.2: Ball
+        # Find current ball position
+        if ball_pos is None:    # If ball was not detected during last cycle, search entire video frame
+            ball_pos = detect_blue_ball(frame)
+        else:                   # If ball was detected last cycle, search only the area surrounding the most recently recorded ball position
+            area_size = 100
+            x_min_offset = min(0, ball_pos[0] - area_size)
+            y_min_offset = min(0, ball_pos[1] - area_size)
+            x_min, x_max = max(0, ball_pos[0] - area_size), ball_pos[0] + area_size
+            y_min, y_max = max(0, ball_pos[1] - area_size), ball_pos[1] + area_size
+            new_ball_pos = detect_blue_ball(frame[y_min:y_max, x_min:x_max, :])
+            
+            # If a ball was successfully detected near prior ball position, calculates new ball position from relative coordinates
+            if new_ball_pos is not None:
+                new_ball_pos = (
+                    max(0, new_ball_pos[0] + ball_pos[0] - area_size - x_min_offset), 
+                    max(0, new_ball_pos[1] + ball_pos[1] - area_size - y_min_offset),
+                    new_ball_pos[2]
+                )
+            ball_pos = new_ball_pos
+
+        # If ball not found during this cycle, tally a detection failure (currently stops at frame 769, as this is when the ball falls into a hole in the training video)
+        # if ball_pos is None and frame_count < 769:
+        if ball_pos is None:
+            missed_frames_ball += 1
+
+
+        ### STEP 3: Draw detected objects and message text to video frame
         
-        # If a ball was successfully detected near prior ball position, calculates new ball position from relative coordinates
-        if new_ball_pos is not None:
-            new_ball_pos = (
-                max(0, new_ball_pos[0] + ball_pos[0] - area_size - x_min_offset), 
-                max(0, new_ball_pos[1] + ball_pos[1] - area_size - y_min_offset),
-                new_ball_pos[2]
-            )
-        ball_pos = new_ball_pos
+        # Draw path
+        if path is not None:
+            cv.drawContours(frame, path, -1, color_map["orange"], 2)
 
-    # If ball not found during this cycle, tally a detection failure (currently stops at frame 769, as this is when the ball falls into a hole in the training video)
-    if ball_pos is None and frame_count < 769:
-        missed_frames_ball += 1
-
-
-    ### STEP 3: Draw detected objects and message text to video frame
-    
-    # Draw path
-    if path is not None:
-        cv.drawContours(frame, path, -1, color_map["orange"], 2)
-
-    # Draw ball position and message text
-    if ball_pos is not None:
-        msg, msg_color = "Ball detected", "green"
-        draw_circles(frame, [ball_pos], BGR_color=color_map["magenta"])
-        draw_text(frame, f"Position (X, Y): {ball_pos[0]}, {ball_pos[1]}", (100, 200), color_map["green"])
-    else:
-        msg, msg_color = "Ball NOT detected", "red"
-    draw_text(frame, msg, (100, 100), color_map[msg_color])
+        # Draw ball position and message text
+        if ball_pos is not None:
+            msg, msg_color = "Ball detected", "green"
+            draw_circles(frame, [ball_pos], BGR_color=color_map["magenta"])
+            draw_text(frame, f"Position (X, Y): {ball_pos[0]}, {ball_pos[1]}", (100, 200), color_map["green"])
+        else:
+            msg, msg_color = "Ball NOT detected", "red"
+        draw_text(frame, msg, (100, 100), color_map[msg_color])
 
 
-    ### Step 4: Display video on screen
-    cv.imshow('Frame', frame)
-    
-    ### Step 5: Check for exit command
-    if cv.waitKey(1) == ord('q'):
-        break
+        ### Step 4: Display video on screen
+        cv.imshow(window_name, frame)
+        
+        ### Step 5: Check for exit command
+        if cv.waitKey(1) == ord('q'):
+            break
 
-cap.release()
-cv.destroyAllWindows()
+    camera.vid.release()
+    cv.destroyAllWindows()
 
-# Print statistics to terminal
-print(f"Number of frames where ball was missed: {missed_frames_ball}")
-print(f"Ball detection rate: {np.around((1 - missed_frames_ball / frame_count), decimals=4) * 100}%")
+    # Print statistics to terminal
+    print(f"Number of frames where ball was missed: {missed_frames_ball}")
+    print(f"Ball detection rate: {np.around((1 - missed_frames_ball / frame_count), decimals=4) * 100}%")
+
+
+if __name__ == "__main__":
+	main()
