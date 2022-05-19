c20a5b2668ab853695e38e3079293e4ba395e455
Andrey-Korn <andreykornilovich@gmail.com>
2022-05-19 10:20:46 -0700

    wip

---
+++ src/path.py
@@ -0,0 +1,13 @@
+# queuing system for navigating through our waypoints representing the path through the maze
+import json
+
+class path(object):
+    pts = []
+
+    def __init__(self, path_file) -> None:
+        pass
+
+    def get_next(self):
+        pass
+
+    
